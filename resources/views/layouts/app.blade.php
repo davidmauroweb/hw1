ff314d528879faf70dd739a2d0613c5021573901
Adolfo David Mauro <davidmauroweb@gmail.com>
2024-11-30 14:47:22 -0300

    Logo en layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Aplicación de Recursos Humanos') }}
+                <img src="{{URL::asset('/mark-b.png')}}" style="width: 9%; height: 9%; border: 2px"> - Inventario
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
